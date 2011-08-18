9f1005981e5ed0d48507193c01ee64a997339c79
Olwethu <olwethu@byteorbit.com>
2011-08-18 12:11:28 +0200

    commit

--- txtalert/txtalert/apps/googledoc/management/commands/import_data.py
+++ txtalert/txtalert/apps/googledoc/management/commands/import_data.py
@@ -2,11 +2,9 @@ from datetime import datetime, timedelta, date
 from django.contrib.auth.models import User
 from django.core.management.base import BaseCommand
 from django.conf import settings
-from txtalert.apps.general.settings.models import DocSettings
 from txtalert.apps.googledoc.importer import Importer
 from txtalert.apps.googledoc.models import SpreadSheet
-from txtalert.core.models import Patient, Visit
-from xml.parsers.expat import ExpatError
+#from txtalert.core.models import Patient, Visit
 import sys, traceback
 import os.path
 import optparse
@@ -19,18 +17,18 @@ class Command(BaseCommand):
     help = 'Can run as Cron job.'
 
     def handle(self, *args, **kwargs):
-        importer = Importer(
+        importer = Importer(ExpatError, e
             uri='https://docs.google.com' % (
                 Setting.objects.get(name='GOOGLE_USERNAME').value,
                 Setting.objects.get(name='GOOGLE_PASSWORD').value
             ),
             verbose=settings.DEBUG
         )
-        for spreadsheet in SpreadSheet.objects.filter(active=True):
+        for spreadsheet in SpreadSheet.objects.all():
             try:
                 importer.import_spread_sheet(spreadsheet)
-            except ExpatError, e:
-                print "Exception during processing XML for clinic ", clinic
+            except e:
+                print "Spreadsheet name exception ", spreadsheet
                 traceback.print_exc()
             
             
